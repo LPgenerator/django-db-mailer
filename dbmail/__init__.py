7f9bece2cf8cb4286f684d040adc504a8972a461
gotlium <gotlium@gmail.com>
2014-07-19 13:21:39 +0500

    djcelery check

--- dbmail/__init__.py
+++ dbmail/__init__.py
@@ -6,13 +6,17 @@ def get_version():
 
 
 def send_db_mail(slug, recipient, *args, **kwargs):
+    from dbmail.defaults import CELERY_QUEUE
     from dbmail.models import MailTemplate
     from dbmail.send_mail import SendMail
-    from dbmail.defaults import CELERY_QUEUE
+    from django.conf import settings
 
     args = (slug, recipient) + args
     try:
         import tasks
+
+        if 'djcelery' not in settings.INSTALLED_APPS:
+            raise ImportError
 
         tasks.send_db_mail.apply_async(
             args=args, kwargs=kwargs, queue=CELERY_QUEUE,
