c09a90f38d884cb38b2200ad0f40ee8ec47f9923
gotlium <gotlium@gmail.com>
2014-12-09 17:11:00 +0300

    update cache command

---
+++ dbmail/management/commands/update_dbmail_cache.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+from dbmail.models import MailTemplate, ApiKey
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        for obj in MailTemplate.objects.all():
+            MailTemplate.get_template(obj.slug)
