d3c1a1d059a9410c8080097c7c86f3b34aa38123
gotlium <gotlium@gmail.com>
2015-03-22 01:05:57 +0500

    fixes on management command

--- dbmail/management/commands/dbmail_test_send.py
+++ dbmail/management/commands/dbmail_test_send.py
@@ -4,17 +4,18 @@ import optparse
 from django.core.management.base import BaseCommand
 
 from dbmail.models import MailTemplate
-from dbmail import send_db_mail
+from dbmail.defaults import BACKEND
+from dbmail import db_sender
 
 
-def send_test_msg(pk, email, user=None, use_celery=True):
+def send_test_msg(pk, email, user=None, **kwargs):
     template = MailTemplate.objects.get(pk=pk)
     slug = template.slug
     var_list = re.findall('\{\{\s?(\w+)\s?\}\}', template.message)
     context = {}
     for var in var_list:
         context[var] = '%s' % var.upper().replace('_', '-')
-    return send_db_mail(slug, email, user, context, use_celery=use_celery)
+    return db_sender(slug, email, user, context, **kwargs)
 
 
 class Command(BaseCommand):
@@ -24,10 +25,24 @@ class Command(BaseCommand):
         optparse.make_option('--without-celery', action='store_true',
                              default=False, dest='celery',
                              help='Send direct message'),
+        optparse.make_option('--provider', dest='provider', help='Provider'),
+        optparse.make_option(
+            '--backend', dest='backend', help='Backend', default='mail'),
     )
+
+    @staticmethod
+    def get_kwargs(options):
+        kwargs = {
+            'use_celery': not options['celery'],
+            'backend': BACKEND['mail']}
+        if options['provider']:
+            kwargs['provider'] = options['provider']
+        if options['backend']:
+            kwargs['backend'] = BACKEND[options['backend']]
+        return kwargs
 
     def handle(self, *args, **options):
         send_test_msg(
-            options['pk'], options['email'], use_celery=not options['celery']
+            options['pk'], options['email'], **self.get_kwargs(options)
         )
         print "Done. Message was sent."
