258a26e5d4463323f538358e9c736a693daea3d3
gotlium <gotlium@gmail.com>
2015-03-23 00:05:47 +0500

    boxcar provider

---
+++ dbmail/providers/boxcar/push.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+
+from httplib import HTTPSConnection
+from urllib import urlencode
+
+from dbmail.providers.prowl.push import from_unicode
+from dbmail import get_version
+
+
+class BoxcarError(Exception):
+    pass
+
+
+def send(token, title, **kwargs):
+    """
+    Site: https://boxcar.io/
+    API: http://help.boxcar.io/knowledgebase/articles/306788-how-to-send-a-notification-to-boxcar-users
+    Desc: Best app for system administrators
+    """
+    headers = {
+        "Content-type": "application/x-www-form-urlencoded",
+        "User-Agent": "DBMail/%s" % get_version(),
+    }
+
+    data = {
+        "user_credentials": token,
+        "notification[title]": from_unicode(title),
+        "notification[sound]": "notifier-2"
+    }
+
+    for k, v in kwargs.items():
+        data['notification[%s]' % k] = from_unicode(v)
+
+    http = HTTPSConnection(kwargs.pop("api_url", "new.boxcar.io"))
+    http.request(
+        "POST", "/api/notifications",
+        headers=headers,
+        body=urlencode(data))
+    response = http.getresponse()
+
+    if response.status != 201:
+        raise BoxcarError(response.reason)
+    return True
