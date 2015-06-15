ec7dfaf47243460d184399c6bb6173f5daf0cdaa
gotlium <gotlium@gmail.com>
2015-06-15 18:35:14 +0300

    demo fix

--- demo/.vagrant_bootstrap.sh
+++ demo/.vagrant_bootstrap.sh
@@ -11,4 +11,4 @@ python /mailer/manage.py syncdb --noinput
 python /mailer/manage.py migrate --noinput
 python /mailer/manage.py loaddata /mailer/auth.json
 
-/bin/bash -c 'python /mailer/manage.py celeryd -Q default >& /dev/null & python /mailer/manage.py runserver 0.0.0.0:8000 >& /dev/null &'
+/bin/bash -c 'C_FORCE_ROOT=1 python /mailer/manage.py celeryd -Q default >& /dev/null & python /mailer/manage.py runserver 0.0.0.0:8000 >& /dev/null &'
