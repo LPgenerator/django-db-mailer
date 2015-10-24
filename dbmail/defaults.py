c4144c5c935a58565249046d3f25d3196180da5a
Volodymyr Tartynskyi <vosi@users.noreply.github.com>
2015-10-24 20:32:04 +0300

    Update defaults.py

--- dbmail/defaults.py
+++ dbmail/defaults.py
@@ -40,7 +40,6 @@ ALLOWED_MODELS_ON_ADMIN = get_settings('DB_MAILER_ALLOWED_MODELS_ON_ADMIN', [
     'MailFromEmailCredential',
     'MailFromEmail',
     'MailCategory',
-    'MailBaseTemplateAdmin',
     'MailTemplate',
     'MailLog',
     'MailGroup',
