770f44e65cd9b06375d476a95c065f7fd9312a7b
lcd1232 <malexey1984@gmail.com>
2017-08-09 12:00:02 +0300

    Fix long line (E501)

--- dbmail/models.py
+++ dbmail/models.py
@@ -719,10 +719,12 @@ class MailLogTrack(models.Model):
     def detect_geo(self):
         if self.ip and self.counter == 0:
             if not TRACK_USE_GEOIP2:
-                from django.contrib.gis.geoip import GeoIP, GeoIPException
+                from django.contrib.gis.geoip import GeoIP
+                from django.contrib.gis.geoip import GeoIPException
             else:
                 from django.contrib.gis.geoip2 import GeoIP2 as GeoIP
-                from django.contrib.gis.geoip2 import GeoIPException2 as GeoIPException
+                from django.contrib.gis.geoip2 import GeoIPException2\
+                as GeoIPException
             try:
                 g = GeoIP()
                 info = g.city(self.ip) or dict()
